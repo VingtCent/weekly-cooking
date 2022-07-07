417a3a34bffdc4e54d7c03afe9d07d551345b8de
Vincent Casal <DS7K@eurofins.com>
2022-07-07 16:31:31 +0200

    cleanup

--- src/views/WeeklyMenuView.vue
+++ src/views/WeeklyMenuView.vue
@@ -1,21 +1,24 @@
 <template>
     <h1>Recettes de la semaine</h1>
-    <v-card>
+    <v-card class="w-33">
         <v-card-item>
             <v-card-title>Ajouter une recette</v-card-title>
         </v-card-item>
-        <v-card-text>
-            <v-autocomplete v-model="selectedRecipy" :items="recipies" item-title="name" item-value="name"></v-autocomplete>
+        <v-card-text dense>
+            <v-autocomplete v-model="selectedRecipy" :items="recipies" item-title="name" item-value="name">
+            </v-autocomplete>
         </v-card-text>
-        <v-card-actions><v-btn @click="add">Add</v-btn></v-card-actions>
-    </v-card>    
+        <v-card-actions>
+            <v-btn @click="add" icon="mdi-plus-circle" color="primary"></v-btn>
+        </v-card-actions>
+    </v-card>
     <v-list :items="weeklyRecipies" item-title="name">
     </v-list>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import { RecipyRepository } from "../repositories/recipyRepository";
-import type {Recipy} from "../repositories/recipyRepository";
+import type { Recipy } from "../repositories/recipyRepository";
 
 export default defineComponent({
     name: 'RecipiesView',
@@ -24,15 +27,15 @@ export default defineComponent({
         recipies: [] as Recipy[],
         weeklyRecipies: [] as Recipy[]
     }),
-    methods:{
-        add(){          
+    methods: {
+        add() {
             var addedRecipy = this.recipies.filter(r => r.name == this.selectedRecipy)[0];
             this.weeklyRecipies.push(addedRecipy);
         }
     },
-    mounted(){
+    mounted() {
         this.recipies = new RecipyRepository().get();
     }
-    
+
 })
 </script>
