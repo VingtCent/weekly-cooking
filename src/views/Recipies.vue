dabb96862feb1a342ded9ed795157c8752ab052d
Vincent Casal <DS7K@eurofins.com>
2022-10-25 18:33:18 +0200

    Recipies tile add

--- src/views/Recipies.vue
+++ src/views/Recipies.vue
@@ -1,12 +1,26 @@
 <template>
     <v-container>
         <v-row>
+            <v-col cols="2">
+                <v-card>
+                    <v-card-title>Ajouter une recette</v-card-title>
+                    <v-card-text></v-card-text>
+                    <v-card-actions><v-btn icon="mdi-book-plus"></v-btn></v-card-actions>
+                </v-card>
+            </v-col>
             <v-col v-for="item in recipies" cols="2">
                 <v-card>
                     <v-card-title>
                         {{ item.name }}
                     </v-card-title>
-                    <v-card-text><a href="{{item.url}}">{{item.url}}</a></v-card-text>
+                    <v-card-text>                        
+                        <v-list v-if="item.ingredients.length > 0">
+                            <v-list-item v-for="ingredient in item.ingredients">{{ingredient.name}}</v-list-item>
+                        </v-list>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn v-if="item.url != null" icon="mdi-earth" :href="item.url"></v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
