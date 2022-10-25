a2109febd8ec7556c4f3745fce3bd2d4c5359a49
Vincent Casal <DS7K@eurofins.com>
2022-10-25 15:41:09 +0200

    Recipies vue

--- src/repositories/recipyRepository.ts
+++ src/repositories/recipyRepository.ts
@@ -1,6 +1,7 @@
 export interface Recipy {
     id: number,
     name: string,
+    url?: string,
     portions: number,
     ingredients: Ingredient[]
 }
@@ -28,6 +29,55 @@ class RecipyRepository implements IRecipyRepository {
             name: 'Pizza',
             portions: 3,
             ingredients: [{ name: 'pate pizza', quantity: 1, unit: '' }]
+        },
+        {
+            id: 3,
+            name: 'Dahl',
+            portions: 3,
+            ingredients: []
+        },
+        {
+            id: 4,
+            name: 'Poireaux vinaigrettes',
+            url: 'https://cuisine.voozenoo.fr/2022/10/20/poireaux-vinaigrette/?utm_source=rss&utm_medium=rss&utm_campaign=poireaux-vinaigrette',
+            portions: 3,
+            ingredients: []
+        },
+        {
+            id: 5,
+            name: 'Roti boeuf',
+            portions: 3,
+            ingredients: []
+        },
+        {
+            id: 6,
+            name: 'Pancakes',
+            portions: 3,
+            ingredients: []
+        },
+        {
+            id: 7,
+            name: 'Galettes',
+            portions: 3,
+            ingredients: []
+        },
+        {
+            id: 8,
+            name: 'Pates aux betteraves',
+            portions: 3,
+            ingredients: []
+        },
+        {
+            id: 9,
+            name: 'Oeufs bacon',
+            portions: 3,
+            ingredients: []
+        },
+        {
+            id: 10,
+            name: 'Rougail',
+            portions: 3,
+            ingredients: []
         }]
     }
 }
---
+++ src/views/Recipies.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col v-for="item in recipies" cols="2">
+                <v-card>
+                    <v-card-title>
+                        {{ item.name }}
+                    </v-card-title>
+                    <v-card-text><a href="{{item.url}}">{{item.url}}</a></v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import recipyRepository from "../repositories/recipyRepository";
+import type { Recipy } from "../repositories/recipyRepository";
+
+export default defineComponent({
+    name: "RecipiesView",
+    data: () => ({
+        recipies: [] as Recipy[],
+    }),
+    mounted() {
+        this.recipies = recipyRepository.get();
+    }
+})
+</script>
